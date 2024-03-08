17876d98a5ec7b4681eadea00ba2c196c3f6466f
RaihanApriansyah <160570626+RaihanApriansyah@users.noreply.github.com>
2024-03-08 22:13:28 +0800

    Add files via upload

---
+++ Tugas 3/Tugas3_SourceCode_Kasus/KasusBoolean.java
@@ -0,0 +1,19 @@
+//NIM = 13020220014
+//Nama = Raihan Apriansyah
+//Hari/Tanggal = Selasa 05/03/2024
+//Waktu Pengerjaan = 14.27 - 14.31
+
+public class KasusBoolean {
+	public static void main(String[] args) {
+	boolean bool;
+	bool= true; 
+	if(bool) {
+		System.out.print("true\n");
+	} else
+		System.out.print("false\n");
+	if(!bool) {
+		System.out.print("salah\n");
+	} else
+		System.out.print("benar\n");
+	}
+} 
