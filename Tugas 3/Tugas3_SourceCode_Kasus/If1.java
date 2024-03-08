ebdbe18f335a32fb6182b1db58dedd947a5a1d08
RaihanApriansyah <160570626+RaihanApriansyah@users.noreply.github.com>
2024-03-08 22:10:41 +0800

    Add files via upload

---
+++ Tugas 3/Tugas3_SourceCode_Kasus/If1.java
@@ -0,0 +1,18 @@
+//NIM = 13020220014
+//Nama = Raihan Apriansyah
+//Hari/Tanggal = Selasa 05/03/2024
+//Waktu Pengerjaan = 14.10 - 14.15
+
+import java.util.Scanner;
+public class If1 {
+
+public static void main(String[] args) {
+Scanner masukan=new Scanner(System.in); 
+int a;
+System.out.print ("Contoh IF satu kasus \n"); 
+System.out.print ("Ketikkan suatu nilai integer : "); 
+a = masukan.nextInt();
+if (a >= 0)
+System.out.print ("\nNilai a positif "+ a);
+	}
+}
---
+++ Tugas 3/Tugas3_SourceCode_Kasus/If3.java
@@ -0,0 +1,22 @@
+//NIM = 13020220014
+//Nama = Raihan Apriansyah
+//Hari/Tanggal = Selasa 05/03/2024
+//Waktu Pengerjaan = 14.20 - 14.26
+
+import java.util.Scanner;
+public class If3{
+	public static void main(String[] args) {
+		int a;
+		Scanner masukan=new Scanner(System.in);
+		System.out.print ("Contoh IF tiga kasus \n"); 
+		System.out.print ("Ketikkan suatu nilai integer :"); 
+		a=masukan.nextInt();
+		if (a > 0){
+			System.out.println ("Nilai a positif "+ a);
+		}else if (a == 0){
+			System.out.println ("Nilai Nol "+ a);
+		}else{
+			System.out.println ("Nilai a negatif "+ a);
+		}
+	}
+}
